f7ab526950a777c0306e44222f56a42644d7a277
Lohran Mendes <lohranfm@gmail.com>
2025-02-27 01:33:20 -0300

    chore: add nodemon as a dev dependency

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ import http from "node:http";
 
 const PORT = 3000;
 
+const rotas = {
+  '/':'Retorno padrão da rota inicial',
+  '/primeira-rota': 'Retorno da primeira rota http',
+  '/segunda-rota': 'Retorno da segunda rota http'
+}
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "content-type": "text/plan" });
-  res.end("Hello World!!!");
+  res.end(rotas[req.url]);
 });
 
 server.listen(PORT, () => {
