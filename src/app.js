8299c23c248f82bc40bfc01910c8eba6dca0cca9
Lohran Mendes <lohranfm@gmail.com>
2025-02-27 14:37:58 -0300

    feat(app.js): add CRUD methods - add C-reate method - add R-ead method - add U-pdate method - add D-elete method

--- src/app.js
+++ src/app.js
@@ -26,9 +26,30 @@ app.get("/livros", (req, res) => {
   res.status(200).json(livros);
 });
 
+app.get('/livros/:id', (req, res) => {
+  const index = searchBook(req.params.id)
+  res.status(200).json(livros[index])
+})
+
 app.post("/livros", (req, res) => {
   livros.push(req.body)
   res.status(201).send("Livro criado!");
 });
+
+app.put('/livros/:id', (req, res) => {
+  const index = searchBook(req.params.id)
+  livros[index].titulo = req.body.titulo;
+  res.status(200).send('Titulo do livro alterado com sucesso!')
+})
+
+app.delete('/livros/:id', (req, res)=>{
+const index = searchBook(req.params.id)
+livros.splice(index, 1)
+res.status(200).send('Livro foi deletado com sucesso!')
+})
+
+function searchBook (id) {
+ return livros.findIndex(livro => livro.id === Number(id))
+}
 
 export default app;
